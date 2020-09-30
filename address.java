4023ed03ee76e67a934b1ae67c0df1925cc14027
sumitsamsharma <sharmasumit94286@gmail.com>
2020-09-30 16:02:00 +0530

    added multiple contact entries option

--- address.java
+++ address.java
@@ -1,26 +1,28 @@
 package com;
+import java.util.ArrayList;
 import java.util.Scanner; 
 class address_update 
 {
 	String first_name,last_name,age,address,city,zip,phone_no,email;
-	String[]database = new String[8];
+	int i=0;
+	String[][]database = new String[100][8];
 	public void display() 
     { 
 		System.out.println("Emplyee details are: ");
 		System.out.println("Name: "+first_name+" "+last_name+"   Age:"+age+"  Address:"+address+" "+city+" "+zip+"  Contact: "+phone_no+" "+email);	
     }
-	public void f_name()
+	public void f_name(int posi)
 	{
 		Scanner myObj= new Scanner(System.in);
 		System.out.println("Enter your first name: ");
 		first_name = myObj.nextLine();
-		database[0]=first_name;
+		database[posi][0]=first_name;
 		System.out.println("Enter your last name:");
 		last_name=myObj.nextLine();
-		database[1]=last_name;
+		database[posi][1]=last_name;
 	}
 	
-	public void input()
+	public void input(int pos)
 	{
 		Scanner myObj = new Scanner(System.in);
 		System.out.println("Enter age: ");
@@ -36,34 +38,39 @@ class address_update
 		System.out.println("Enter email_id: ");
 		email=myObj.nextLine();
 		
-		database[2]=age;
-		database[3]=address;
-		database[4]=city;
-		database[5]=zip;
-		database[6]=phone_no;
-		database[7]=email;
-  
+		database[pos][2]=age;
+		database[pos][3]=address;
+		database[pos][4]=city;
+		database[pos][5]=zip;
+		database[pos][6]=phone_no;
+		database[pos][7]=email;
+		System.out.println("  "+database);
+
 	}
 	
-	public int search (String name)
+	public int search (String name,int count)
 	{
-		if(database[0].equals(name))
-	    {
+		
+		int i=count-1;
+		while(i>=0)
+		{	
+		   if(database[i][0].equals(name))
+	      {
 	    	System.out.println("Name found");
-	    	return 1;
-	    }
-	    else 
-	    	System.out.println("Name not found");
-	    	return 0;
-	    
+	    	return i;
+	      }
+		   i=i-1;
+		}
+	    System.out.println("Name not found");
+	    return 2000;
 	}
-	public void erase()
+	
+	public void erase(int pos)
 	{
-	    for(int j=0;j<8;j++)
-	    {
-	    	database[j]=" ";
-	    }
-	    
+		 for(int j=0;j<8;j++)
+	     {
+	       	database[pos][j]=" ";
+	     }  
 	}
 
 	
@@ -75,28 +82,32 @@ public class address
 		Scanner obj3 = new Scanner(System.in);
 		address_update person=new address_update();
 		int end=1;
+		int total=0;
 		while(end!=0)
 		{
 			System.out.println("Enter your choice: 1.New Contact"+" 2.Edit existing contact"+ " 3.Delete a contact ");
 			int choice=obj3.nextInt();
+			
 			switch(choice)
 		   {
 	        case 1:
-	        	
 	        	System.out.println("Enter details");
-	        	person.f_name();
-	    		person.input();
-	    		person.display();	
+	        	person.f_name(total);
+	    		person.input(total);
+	    		total++;
+	    		person.display();
+	    		System.out.println("Total "+total);
 	        	break;
+	        	
 	        case 2:
 	        	System.out.println("Enter first name of the person for editing info");
 	        	Scanner obj1 = new Scanner(System.in);
 	        	String name=obj1.nextLine();
-	        	int x=person.search(name);
-	        	if(x==1)
+	        	int x=person.search(name,total);
+	        	if(x!=2000)
 	        	{	
-	        	  System.out.println("Edit");
-	        	  person.input();
+	        	  System.out.println("Editting now.....");
+	        	  person.input(x);
 	        	} 
 	        	break;
 	        	       	
@@ -104,10 +115,10 @@ public class address
 	        	System.out.println("Enter first name of the person for deleting");
 	        	Scanner obj2 = new Scanner(System.in);
 	        	String f_name=obj2.nextLine();
-	        	int y=person.search(f_name);
-	        	if(y==1)
+	        	int y=person.search(f_name,total);
+	        	if(y!=2000)
 	        	{	
-	        	  person.erase();	
+	        	  person.erase(y);	
 	        	  System.out.println("Deleted.");
 	        	}
 	        	break;
